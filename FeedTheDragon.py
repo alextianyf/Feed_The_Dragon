ea323b3dde53232333a6013addd8796e73efc4fd
Alex Tian <70412334+alextianyf@users.noreply.github.com>
2024-03-27 22:48:03 -0700

    load image

--- FeedTheDragon.py
+++ FeedTheDragon.py
@@ -1,39 +1,23 @@
 import pygame
 
-#Intiailize Pygame
+#Initialize pygame
 pygame.init()
 
-#Create a display surface and set its caption
+#Create a display surface
 WINDOW_WIDTH = 600
-WINDOW_HEIGHT = 600
+WINDOW_HEIGHT = 300
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption("Drawing Objects")
+pygame.display.set_caption("Blitting Images!")
 
-#Define colors as RGB tuples
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-CYAN = (0, 255, 255)
-MAGNETA = (255, 0, 255)
+#Create images...returns a Surface object with the image drawon on it.
+#We can then get the rect of the surface and use the rect to position the image.
+dragon_left_image = pygame.image.load("dragon_left.png")
+dragon_left_rect = dragon_left_image.get_rect()
+dragon_left_rect.topleft = (0,0)
 
-#Give a background color to the display
-display_surface.fill(BLUE)
-
-#Draw various shapes on our display
-#Line(surface, color, starting point, ending point, thickness)
-pygame.draw.line(display_surface, RED, (0,0), (100,100), 5)
-pygame.draw.line(display_surface, GREEN, (100,100), (200,300), 1)
-
-#Circle(surface, color, center, radius, thickness...0 for fill)
-pygame.draw.circle(display_surface, WHITE, (WINDOW_WIDTH//2, WINDOW_HEIGHT//2), 200, 6)
-pygame.draw.circle(display_surface, YELLOW, (WINDOW_WIDTH//2, WINDOW_HEIGHT//2), 180, 0)
-
-#Rectangle(surface, color, (top-left x, top-left y, width, height))
-pygame.draw.rect(display_surface, CYAN, (500, 0, 100, 100))
-pygame.draw.rect(display_surface, MAGNETA, (500, 100, 50, 100))
+dragon_right_image = pygame.image.load("dragon_right.png")
+dragon_right_rect = dragon_right_image.get_rect()
+dragon_right_rect.topright = (WINDOW_WIDTH, 0)
 
 #The main game loop
 running = True
@@ -41,6 +25,12 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    #Blit (copy) a surface object at the given coordinates to our display
+    display_surface.blit(dragon_left_image, dragon_left_rect)
+    display_surface.blit(dragon_right_image, dragon_right_rect)
+
+    pygame.draw.line(display_surface, (255, 255, 255), (0, 75), (WINDOW_WIDTH, 75), 4)
 
     #Update the display
     pygame.display.update()
