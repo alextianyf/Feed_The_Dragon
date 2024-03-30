846afdb4742cb1085baa531bc75fe72abf700ea6
Alex Tian <70412334+alextianyf@users.noreply.github.com>
2024-03-30 14:40:24 -0700

    Keyboard Discrete Movement

--- FeedTheDragon.py
+++ FeedTheDragon.py
@@ -1,44 +1,50 @@
 import pygame
 
-#Intialize pygame
+#Initialize pygame
 pygame.init()
 
-#Create a display surface
+#Create our display surface
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 300
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption("Adding Sounds!")
+pygame.display.set_caption("Discrete Movement!")
 
-#Load sound effects
-sound_1 = pygame.mixer.Sound('sound_1.wav')
-sound_2 = pygame.mixer.Sound('sound_2.wav')
+#Set game values
+VELOCITY = 30
 
-#Play the sound effects
-sound_1.play()
-pygame.time.delay(2000)
-sound_2.play()
-pygame.time.delay(2000)
-
-#Change the volume of a sound effect
-sound_2.set_volume(.1)
-sound_2.play()
-
-#Load background music
-pygame.mixer.music.load('music.wav')
-
-#Play and stop the music
-pygame.mixer.music.play(-1, 0.0)
-pygame.time.delay(1000)
-sound_2.play()
-pygame.time.delay(5000)
-pygame.mixer.music.stop()
+#Load in images
+dragon_image = pygame.image.load("dragon_right.png")
+dragon_rect = dragon_image.get_rect()
+dragon_rect.centerx = WINDOW_WIDTH//2
+dragon_rect.bottom = WINDOW_HEIGHT
 
 #The main game loop
 running = True
 while running:
     for event in pygame.event.get():
+        print(event)
         if event.type == pygame.QUIT:
             running = False
+
+        #Check for discrete movement
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                dragon_rect.x -= VELOCITY
+            if event.key == pygame.K_RIGHT:
+                dragon_rect.x += VELOCITY
+            if event.key == pygame.K_UP:
+                dragon_rect.y -= VELOCITY
+            if event.key == pygame.K_DOWN:
+                dragon_rect.y += VELOCITY
+
+    #Fill the display surface to cover old images
+    display_surface.fill((0, 0, 0))
+
+    #Blit (copy) assets to the screen
+    display_surface.blit(dragon_image, dragon_rect)
+
+    #Update the display
+    pygame.display.update()
 
 #End the game
 pygame.quit()
