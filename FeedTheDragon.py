8c16b0782f2b0399b365e76cf02103ad34d225a9
Alex Tian <70412334+alextianyf@users.noreply.github.com>
2024-03-30 20:23:43 -0700

    Continuous Keyboard Movement

--- FeedTheDragon.py
+++ FeedTheDragon.py
@@ -1,18 +1,25 @@
 import pygame
 
-#Initiailze Pygame
+#Initialize pygame
 pygame.init()
 
 #Create a display surface
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 300
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption("Mouse Movement!")
+pygame.display.set_caption("Continuous Movement!")
+
+#Set FPS and clock
+FPS = 60
+clock = pygame.time.Clock()
+
+#Set game values
+VELOCITY = 5
 
 #Load images
 dragon_image = pygame.image.load("dragon_right.png")
 dragon_rect = dragon_image.get_rect()
-dragon_rect.topleft = (25, 25)
+dragon_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
 
 #The main game loop
 running = True
@@ -21,22 +28,20 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        #Move based on mouse clicks
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            print(event)
-            mouse_x = event.pos[0]
-            mouse_y = event.pos[1]
-            dragon_rect.centerx = mouse_x
-            dragon_rect.centery = mouse_y
+    #Get a list of all keys currently being pressed down
+    keys = pygame.key.get_pressed()
+    print(keys)
 
-        #Drag the object when the mouse button is clicked
-        if event.type == pygame.MOUSEMOTION and event.buttons[0] == 1:
-            print(event)
-            mouse_x = event.pos[0]
-            mouse_y = event.pos[1]
-            dragon_rect.centerx = mouse_x
-            dragon_rect.centery = mouse_y
-
+    #Move the dragon continuously
+    if keys[pygame.K_LEFT]:
+        dragon_rect.x -= VELOCITY
+    if keys[pygame.K_RIGHT]:
+        dragon_rect.x += VELOCITY
+    if keys[pygame.K_UP]:
+        dragon_rect.y -= VELOCITY
+    if keys[pygame.K_DOWN]:
+        dragon_rect.y += VELOCITY
+    
     #Fill the display
     display_surface.fill((0, 0, 0))
 
@@ -45,6 +50,9 @@ while running:
 
     #Update the display
     pygame.display.update()
+
+    #Tick the clock
+    clock.tick(FPS)
 
 #End the game
 pygame.quit()
